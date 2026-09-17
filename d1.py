e15d1c699ba11987e4d16478871883e181535536
sakshi-2708 <savaliyasakshi53@gmail.com>
2026-09-17 07:35:27 +0000

    day1

---
+++ d1.py
@@ -0,0 +1,22 @@
+a=5
+print(a)
+
+a,b,c= 1,2,3
+print(a,b,c)
+
+print(a:=2)
+print(a)
+
+a=[1,"sakshi",True,2,3,4,5]
+print(a)
+
+x=y=z="orange"
+print(x)
+print(y)
+print(z)
+
+fuirts=["apple","banana","cherry"]
+x,y,z=fuirts
+print(x)
+print(y)
+print(z)
